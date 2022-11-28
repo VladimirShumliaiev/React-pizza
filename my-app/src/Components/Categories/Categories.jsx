daf7a8a01cc79734cf0786cd14bd26752b0809bc
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2022-11-28 17:50:34 +0200

    +++28.11.2022 Pizza

--- my-app/src/Components/Categories/Categories.jsx
+++ my-app/src/Components/Categories/Categories.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Categories = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
     const categoriesList = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
+
     return (
         <div className="categories">
             <ul>
                 {
-                    categoriesList.map(e => <li className={'active'}>{e}</li>)
+                    categoriesList.map((e,index) => <li onClick={() => setActiveIndex(index)} className={activeIndex === index ? 'active' : ''}>{e}</li>)
                 }
             </ul>
         </div>
